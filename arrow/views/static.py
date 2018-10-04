960c4d5923fcd654ecba6c3ff7486f9d932e9a32
Anton Petrov <le4dof@gmail.com>
2018-10-04 02:24:12 -0700

    added mime types and file reader

--- arrow/views/static.py
+++ arrow/views/static.py
@@ -5,15 +5,21 @@ import os, os.path
 class View(arrow.view):
 
     def get(self, req, res):
-        static_path = self.view_params.get('static_path')
-        filepath = os.path.join(os.getcwd(), static_path, self.route.get('filename'))
+        static_path = os.path.join(os.getcwd(), self.view_params.get('static_path'))
+        exclude = self.view_params.get('exclude')
+        filepath = os.path.join(static_path, self.route.get('filename'))
+        for exclude_path in exclude:
+            if filepath.startswith(exclude_path):
+                return res.abort(400)
 
         if not os.path.exists(filepath):
+            return res.abort(404)
+
+        if not filepath.startswith(static_path):
             return res.abort(400)
 
-        if not filepath.startswith(os.path.join(os.getcwd(), static_path)):
-            return res.abort(400)
-            
+        res.header('Content-Type', arrow.mime.get(os.path.splitext(filepath)[1], ''))
+
         with open(filepath, 'rb') as f:
             res.binary(f.read())
             return res.send()
